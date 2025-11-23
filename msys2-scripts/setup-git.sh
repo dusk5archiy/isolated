c437b88063d0154686874de3bd0bc9e19fef917d
Dusk Sarchiy <dusqdn.eml@gmail.com>
2025-11-23 22:37:41 +0700

    add configs to setup git script, and add .gitkeep to PATH

--- msys2-scripts/setup-git.sh
+++ msys2-scripts/setup-git.sh
@@ -4,3 +4,6 @@ read -p "Email: " email
 touch $HOME/.gitconfig
 git config --global user.name "$name"
 git config --global user.email "$email"
+git config --global safe.directory *
+git config --global init.defaultbranch main
+git config --global credential.helperselector.selected "<no helper>"
